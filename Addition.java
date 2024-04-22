95fa7b95f80a02b15038142558266651810ea3d7
Shubham Chaudhary <99864100+Shubham-72654@users.noreply.github.com>
2024-04-22 22:50:41 +0530

    Addition of 2 numbers
    Basic program for addition of 2 numbers

--- Addition.java
+++ Addition.java
@@ -1,18 +1,13 @@
 // Addition of two number basic.
-
 import java.lang.*;
-
 class Addition {
     // entry point function
     public static void main(String arg[]){
-
         // variable initialization
         int addition = 0;
         int a = 10;
         int b = 11;
-
         addition = a + b;
-
         System.out.println("Addition of " + a + " and " + b + " is : " + addition);
     }
 }
